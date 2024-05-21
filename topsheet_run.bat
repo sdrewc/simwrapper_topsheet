21a603a5e6265c60db913bd229b173b82722850d
weiwang108 <146976419+weiwang108@users.noreply.github.com>
2024-05-20 17:58:17 -0700

    one click and run v6

--- topsheet_run.bat
+++ topsheet_run.bat
@@ -5,23 +5,26 @@ echo Running Python scripts...
 cd scripts
 
 REM Create the temporary conda environment
-CALL conda create --name topsheet -c conda-forge python=3.11 -y
+CALL conda create --name topsheet_data -c conda-forge python=3.11 -y
 
 REM Install pip in the environment
-CALL conda run --name topsheet conda install pip -y
+CALL conda run --name topsheet_data conda install pip -y
 
 REM Install requirements using pip
-CALL conda run --name topsheet python -m pip install -r requirements.txt
+CALL conda run --name topsheet_data python -m pip install -r requirements.txt
 
 :: Run each Python script
-CALL conda run --name topsheet python landuse.py
-CALL conda run --name topsheet python mode.py
-CALL conda run --name topsheet python rpt.py
-CALL conda run --name topsheet python vmt_vht.py
-CALL conda run --name topsheet python transit.py
-CALL conda run --name topsheet python traffic.py
+CALL conda run --name topsheet_data python landuse.py
+CALL conda run --name topsheet_data python mode.py
+CALL conda run --name topsheet_data python rpt.py
+CALL conda run --name topsheet_data python vmt_vht.py
+CALL conda run --name topsheet_data python transit.py
+CALL conda run --name topsheet_data python traffic.py
 
-CALL conda env remove --name topsheet -y
+:: Pause for 10 seconds to ensure all file operations are completed
+timeout /t 10 /nobreak
+
+CALL conda env remove --name topsheet_data -y
 
 echo Scripts execution completed.
 pause
