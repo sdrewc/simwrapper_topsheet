7c1f47008f41b293a599d2ae5b51fe8930a76423
Drew Cooper <Drew@sfcta.org>
2024-06-14 17:44:38 -0700

    update run/control files
    Signed-off-by: Drew Cooper <Drew@sfcta.org>

--- topsheet_run.bat
+++ topsheet_run.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+set CHAMPVERSION=Y:\champ\dev\7.0.0_dev_25w
 :: Path to the configuration file
 set "CONFIG_FILE=%~dp0folders_config.txt"
 
@@ -21,36 +22,23 @@ echo Output Folder: %OUTPUT_FOLDER%
 echo Scripts Folder: %SCRIPTS_FOLDER%
 
 :: Change to the scripts directory
-cd /d %SCRIPTS_FOLDER%
+:: cd /d %SCRIPTS_FOLDER%
 
-:: Check if the Conda environment exists
-FOR /F "tokens=*" %%i IN ('conda info --envs ^| findstr /C:"topsheet_data"') DO SET ENV_EXISTS=%%i
-
-:: Create the environment if it does not exist
-IF NOT DEFINED ENV_EXISTS (
-    CALL conda create --name topsheet_data -c conda-forge python=3.11 -y
-    CALL conda run --name topsheet_data conda install pip -y
-    CALL conda run --name topsheet_data python -m pip install -r %CONFIG_FILE%\requirements.txt
-) ELSE (
-    ECHO Environment already exists.
-)
-
-:: Example of script execution (Assume running a Python script as an example)
 echo Running scripts in %SCRIPTS_FOLDER%...
 
 :: Run each Python script
 echo Running landuse
-CALL conda run --name topsheet_data python landuse.py
+CALL python %SCRIPTS_FOLDER%\landuse.py
 echo Running mode
-CALL conda run --name topsheet_data python mode.py
+CALL python %SCRIPTS_FOLDER%\mode.py
 echo Running rpt, it will take about 30 minutes
-CALL conda run --name topsheet_data python rpt.py
+CALL python %SCRIPTS_FOLDER%\rpt.py
 echo Running vmt_vht
-CALL conda run --name topsheet_data python vmt_vht.py
+CALL python %SCRIPTS_FOLDER%\vmt_vht.py
 echo Running transit
-CALL conda run --name topsheet_data python transit.py
+CALL python %SCRIPTS_FOLDER%\transit.py
 echo Running traffic
-CALL conda run --name topsheet_data python traffic.py
+CALL python %SCRIPTS_FOLDER%\traffic.py
 
 endlocal
 
